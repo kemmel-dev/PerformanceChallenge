afcd5d68f560ff9fde6978b92406e3c6de7f5174
Kamiel <kamieldevisser@gmail.com>
2022-09-21 11:50:02 +0200

    init commit

---
+++ Assets/Scripts/BeeGenScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeGenScript : MonoBehaviour {
+
+    public GameObject beePrefab;
+    
+    public GameObject[] bee;
+    public int nBees = 200;
+
+    public float spacing = .2f;
+
+    public GameObject queen;
+    float ph = 0;
+
+    public Vector2 bounds;
+
+    // Start is called before the first frame update
+    void Start() {
+        //declare at BlackBoard
+        BB.bees = this;
+
+        float tx, ty;
+
+        bee = new GameObject[nBees];
+
+        for (int i = 0; i < nBees; i++) {
+            tx = Random.Range(0.1f, 10f);
+            ty = Random.Range(0.1f, 10f);
+            bee[i] = Instantiate(beePrefab, new Vector3(tx, 2, ty), Quaternion.identity);
+        }     
+
+        
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(bounds.x * 2, .25f, bounds.y * 2));
+    }
+
+    void FixedUpdate() {
+        //move Queen Bee
+        ph += .005f;
+        queen.transform.position = new Vector3(5f+ Mathf.Sin(ph * 3), 1.2f+Mathf.Sin(ph * 4), 5f+Mathf.Sin(ph * 5)) ;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            BuzzBees(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            BuzzBees(false);
+        }
+    }
+
+    private void BuzzBees(bool buzz)
+    {
+        foreach (GameObject b in bee)
+        {
+            b.SetActive(buzz);
+        }
+    }
+}
---
+++ Assets/Scripts/PineGen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineGen : MonoBehaviour {
+
+    public GameObject pinePrefab;
+    GameObject[] pine;
+    int nPines = 5000;
+
+    public Vector2 bounds;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        float tx, ty;
+
+        pine = new GameObject[nPines];
+
+        for (int i = 0; i < nPines; i++) {
+            tx = Random.Range(-bounds.x, bounds.x);
+            ty = Random.Range(-bounds.y, bounds.y);
+            pine[i] = Instantiate(pinePrefab, new Vector3(tx, 0, ty) + this.transform.position, Quaternion.identity);
+            pine[i].transform.localScale *= Random.Range(.5f, 1.5f);
+            pine[i].transform.parent = this.transform;
+
+        }     
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            this.transform.position = new Vector3(-transform.position.x, transform.position.y, -transform.position.z);
+        }
+    }
+
+}
